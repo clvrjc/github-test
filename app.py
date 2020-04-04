56fe29f913dbc9a7e3da0db8e26883073e87f3e7
clvrjc <50040967+clvrjc@users.noreply.github.com>
2020-04-05 01:55:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
+'''
 open("./index.html", "x")
 with open("index.html", "w") as file:
     f.write("<html><head></head><body><h1>TEST</h1></body></html>")
     f.close()
     
 print("good")
+'''
+from git import Repo
+
+repo_dir = 'github-test'
+repo = Repo(repo_dir)
+file_list = [
+    'C:\Users\User\Downloads\templates\Blog\static\images\jear.jpg'
+]
+commit_message = 'Add simple regression analysis'
+repo.index.add(file_list)
+repo.index.commit(commit_message)
+origin = repo.remote('origin')
+origin.push()
