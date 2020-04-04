2240ac06804811e53aa0faaf323f7c84a1afa3b8
clvrjc <50040967+clvrjc@users.noreply.github.com>
2020-04-05 03:03:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,39 @@ GITHUB_USER = os.environ['GITHUB_USER']
 GITHUB_PASSWORD = os.environ['GITHUB_PASSWORD']
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
-# using username and password
-g = Github(GITHUB_USER, GITHUB_PASSWORD)
 
-# or using an access token
-g = Github(GITHUB_TOKEN)
 
-# Github Enterprise with custom hostname
-g = Github(base_url="https://je-becerro.herokuapp.com//api/v3", login_or_token=GITHUB_TOKEN)
+#Libraries to be import START
+import random
+from flask import Flask, request, g, session, render_template, redirect, url_for, flash
+import os
+import json
+import time, string, random, re
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    repo.edit(has_wiki=False)
-    # to see all the available attributes and methods
-    print(dir(repo))
+#import Mongo#import Mongo.py
+#from NLU import nlp
+#from collections import Counter #install collections
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    # using username and password
+    g = Github(GITHUB_USER, GITHUB_PASSWORD)
+
+    # or using an access token
+    g = Github(GITHUB_TOKEN)
+
+    # Github Enterprise with custom hostname
+    g = Github(base_url="https://je-becerro.herokuapp.com//api/v3", login_or_token=GITHUB_TOKEN)
+
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+        repo.edit(has_wiki=False)
+        # to see all the available attributes and methods
+        print(dir(repo))
+
+if __name__ == "__main__":
+	app.run(debug=True,port='5050')
+ 
+
