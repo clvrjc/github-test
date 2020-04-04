0012f9b3d2050f361ff279d00eede080b991b5bc
clvrjc <50040967+clvrjc@users.noreply.github.com>
2020-04-04 22:08:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 open("./index.html", "x")
 with open("index.html", "w") as file:
     f.write("<html><head></head><body><h1>TEST</h1></body></html>")
-  f.close()
+    f.close()
     
 print("good")
