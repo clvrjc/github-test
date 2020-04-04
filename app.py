d7eae9605c650aec928a9c5ccb011c2732c58398
clvrjc <50040967+clvrjc@users.noreply.github.com>
2020-04-04 21:52:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-import git
-import os
 
-repo_dir = os.path.join(rw_dir, 'github-test')
-file_name = os.path.join(repo_dir, 'index.html')
-print("file created")
-r = git.Repo.init(repo_dir)
 # This function just creates an empty file ...
-content = open(file_name, 'wb')
-content.write("<html><head></head><body><h1>TEST</h1></body></html>")
-content.close()
-print("file written")
-r.index.add([file_name])
-r.index.commit("initial commit")
-print("repository commited")
+f=open("index.html", "wr")
+f.write("<html><head></head><body><h1>TEST</h1></body></html>")
+f.close()
+
+print("good")
