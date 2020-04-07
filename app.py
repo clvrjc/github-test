05225cc12ef1439e7ffa27df6b74b2cd7cc5b1d8
clvrjc <50040967+clvrjc@users.noreply.github.com>
2020-04-08 03:48:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import os
 import json
 import time, string, random, re
 from github import Github
+from hashlib import sha1
 
 GITHUB_USER = os.environ['GITHUB_USER']
 GITHUB_PASSWORD = os.environ['GITHUB_PASSWORD']
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
+
+#---------------------------------------------------------------------------------------------
 #Using username and password to connect
 #g = Github(GITHUB_USER, GITHUB_PASSWORD)
 	
@@ -18,11 +21,12 @@ g = Github(GITHUB_TOKEN)
 	
 #if it is Github enterprise with costum hostname
 #g = Github(base_url="https://je-becerro.herokuapp.com//api/v3", login_or_token=GITHUB_TOKEN)
+#-----------------------------------------------------------------------------------------------
 
 #Create a new file in the repository
 repo = g.get_repo("clvrjc/github-test")
 repo.create_file("test.txt", "comment", "content inside", branch="master")#master is a default branch
-{'content': ContentFile(path="test.txt"), 'commit': Commit(sha="")}	
+{'content': ContentFile(path="test.txt"), 'commit': Commit(sha=sha1_('test'))}	
 
 '''
 #Update a file in the repository
@@ -48,8 +52,13 @@ def index():
 	repo = g.get_repo("clvrjc/github-test")
 	contents = repo.get_contents("test.txt", ref="master")
 	repo.update_file(contents.path, "another comment", "gwap ko insde and out", contents.sha, branch="master")
-	{'commit': Commit(sha=GITHUB-TOKEN), 'content': ContentFile(path="test.txt")}
+	{'commit': Commit(sha=sha1_('test')), 'content': ContentFile(path="test.txt")}
+	
 	return "ok"
+
+def sha1_(var):
+	m = sha1(var.encode('utf-8'))
+	return m.hexdigest()
 
 if __name__ == "__main__":
 	app.run()
